c493b079f3183ec088d3c945cd2bc9de70fedb21
Soubhik Samanta <119947352+Soubhik19@users.noreply.github.com>
2025-10-14 18:47:37 +0000

    Fix Node server to listen on 0.0.0.0 for Docker connectivity
    - Changed app.listen() to bind to 0.0.0.0 instead of localhost
    - This allows connections from outside the Docker container
    - Fixes GitHub Actions test that connects to localhost:8001
    - Server now properly accessible when running in Docker

--- my-express-server/src/app.js
+++ my-express-server/src/app.js
@@ -24,6 +24,6 @@ app.get('/tasks', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
